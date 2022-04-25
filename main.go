d791c0af3643270da4d98cf9f2c7913de8c3cafe
Jacob Smith <jsmithdenverdev@gmail.com>
2022-04-25 17:05:57 -0600

    Breaking the code

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Println("Author: Jake Smith")
+	fmt.Println("Author: Jake Smith"
 	fmt.Println("App: Example")
 	fmt.Println("Version: 1.0.0")
 
