86e1ed42b1389df62bb3a7d0c34e459a19bac560
Jacob Smith <jsmithdenverdev@gmail.com>
2022-04-25 16:23:41 -0600

    Added example cli

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Author: Jake Smith")
+	fmt.Println("App: Example")
+	fmt.Println("Version: 1.0.0")
+
+	fmt.Print("Enter your name: ")
+	name, _ := reader.ReadString('\n')
+
+	fmt.Printf("Hello, %s\n", name)
+}
